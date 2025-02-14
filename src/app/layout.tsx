8404abbe0dddd437dd1dac5d653a37b6dc7f1318
rizkinov <148356406+rizkinov@users.noreply.github.com>
2025-02-14 19:50:25 +0800

    Create layout.tsx
    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,10 @@
+import { GeistSans, GeistMono } from "next/font/geist";
+import "./globals.css";
+
+const sans = GeistSans({
+  subsets: ["latin"]
+});
+
+const mono = GeistMono({
+  subsets: ["latin"]
+});
